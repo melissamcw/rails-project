af8d3c1d51c1e253aacb0e1c40178c097b26bf5e
melissamcw <melissa.mcw@gmail.com>
2014-04-05 12:23:57 -0500

    Adding new files for new Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgirls::Application.config.secret_key_base = '1e43c764a35703ac22a30f6729b70a9a921ff3395f6d8ddbe8d2727afe0899911c9ad4f061c522d585be1faf42d2a4f189549cba3f9314aee8cc8d116abc90f4'
